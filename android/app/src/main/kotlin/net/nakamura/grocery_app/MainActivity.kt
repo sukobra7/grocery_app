b0323696cc1bf90a4001d4b28af31fdcefd4172e
kazumasa.nakamura <kazumasa.nakamura@vitalize.co.jp>
2021-06-15 08:39:47 +0900

    Initial commit build development environment

---
+++ android/app/src/main/kotlin/net/nakamura/grocery_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.nakamura.grocery_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+// void main() async {
+//
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp();
+//   runApp(App());
+// }
+//
+// class App extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//
+//     return MaterialApp(
+//         title: "Grocery App",
+//         home: StoreListPage(),
+//         theme: ThemeData(
+//             primaryColor: Colors.green
+//         )
+//     );
+//
+//   }
+// }
